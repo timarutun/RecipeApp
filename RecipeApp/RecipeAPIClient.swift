3751e3f1917f2b2369924005f1355ca40635f150
TimberSoup <157195566+TimberSoup@users.noreply.github.com>
2025-02-18 20:07:19 -0500

    Add RecipeAPIClient for fetching recipes

---
+++ RecipeApp/RecipeAPIClient.swift
@@ -0,0 +1,31 @@
+//
+//  RecipeAPIClient.swift
+//  RecipeApp
+//
+//  Created by Timur on 2/18/25.
+//
+
+import Foundation
+
+struct RecipeAPIClient {
+    
+    private let recipesURL = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json")!
+    
+    //MARK: Fetching Recipes
+    func fetchRecipes() async throws -> [Recipe] {
+        let (data, response) = try await URLSession.shared.data(from: recipesURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+            else {
+                throw URLError(.badServerResponse)
+            }
+        
+        let decodedData = try JSONDecoder().decode([String: [Recipe]].self, from: data)
+        
+        guard let recipes = decodedData["recipes"] else {
+            throw URLError(.cannotDecodeContentData)
+        }
+        
+        return recipes
+    }
+}
